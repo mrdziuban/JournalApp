0263fb6cb0ab81d10f16c51fcf0fc0274bb0c163
App Academy Student <student@appacademy.io>
2013-07-29 17:41:10 -0400

    Adds double click editing on post show page.

--- app/assets/javascripts/views/posts_show_view.js
+++ app/assets/javascripts/views/posts_show_view.js
@@ -1,4 +1,10 @@
 Journl.Views.PostsShowView = Backbone.View.extend({
+  events: {
+    'dblclick .title' : 'editTitle',
+    'dblclick .body' : 'editBody',
+    'click #save' : 'save'
+  },
+
   render: function () {
     var that = this;
 
@@ -8,5 +14,31 @@ Journl.Views.PostsShowView = Backbone.View.extend({
 
     that.$el.html(renderedContent);
     return that;
+  },
+
+  editTitle: function(e) {
+    var title = this.model.get("title");
+    $(e.toElement).html("<input type='text' value='"+title+"' class='editedTitle'>");
+  },
+
+  editBody: function(e) {
+    var body = this.model.get("body");
+    $(e.toElement).html("<textarea>"+body+"</textarea>");
+  },
+
+  save: function(e) {
+    var title = $("input[type=text]").val();
+    var body = $("textarea").val();
+    if (title === undefined) {
+      title = this.model.get("title");
+    }
+    if (body === undefined) {
+      body = this.model.get("body");
+    }
+    this.model.set({title: title ,body: body})
+    this.model.save();
+    $(".title").html(this.model.get("title"));
+    $(".body").html(this.model.get("body"));
+    console.log("saved");
   }
 });
